e8b68a47fab9c543ea99bf3afb2573d268f44221
Jack <44752360+Jack-n-21@users.noreply.github.com>
2018-11-23 12:49:16 +0200

    adjusting personal data

--- SERVER/data/parcelData.js
+++ SERVER/data/parcelData.js
@@ -2,27 +2,25 @@ const parcels=[
     {
         id:"1",
         userId:"11",
-        origin:"Kigali",
-        destination:"Ruhengeri",
+        origin:"Nigeria",
+        destination:"Kigali",
         weight:"20",
-        createdDate:"1/1/2018",
+        
         status:"pending",
     },{
         id:"2",
         userId:"12",
-        origin:"Muhanga",
-        destination:"Rwamagana",
+        origin:"Uganda",
+        destination:"Kenya",
         weight:"24",
-        createdDate:"1/09/2018",
         status:"pending",
     },
     {
         id:"3",
         userId:"13",
-        origin:"Gisenyi",
-        destination:"Nyamata",
+        origin:"Rwanda",
+        destination:"Nigeria",
         weight:"30",
-        createdDate:"1/07/2018",
         status:"pending",
     }
 ]
