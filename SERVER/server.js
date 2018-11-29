c08007dc4a82e5793a4e6376c9a9503e2d0d2412
Jack <44752360+Jack-n-21@users.noreply.github.com>
2018-11-29 06:04:25 +0200

    adjusting endpoints

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
 import bodyParser from 'body-parser';
 import parcelRoutes from './routes/parcelRoutes';
 import userRoutes from './routes/userRoutes'
 import logger from 'morgan';
+const jwt = require('jsonwebtoken')
 
 const app = express();
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -14,11 +16,59 @@ app.use(bodyParser.urlencoded({
 app.use('/api/v1/parcels', parcelRoutes);
 app.use('/api/v1/users', userRoutes);
 
-app.get('/',(req,res)=>{
-  res.send({
-    message:"welcome"
-  })
-})
+
+app.post('/api/login', verifyToken, ( req, res)=>{
+
+  jwt.verify(req.token, 'secretkey', (err, authData)=>{
+    if(err){
+      res.sendStatus(403);
+    } else{
+      res.json({
+        message: 'post created..',
+        authData
+      });
+    }
+
+    });
+  });
+ 
+
+
+
+  // authentication
+// app.post('/api/login', verifyToken, ( req, res)=>{
+  // user
+
+  const user= {
+    id:1,
+    username:'jack',
+    email:'user@gmail.com'
+  }
+  jwt.sign({user}, 'secretkey', (err, token)=>{
+    res.json({
+      token
+    });
+  });
+
+// verify token 
+function verifyToken(req, res, next){
+
+  const bearerHeader = req.headers['authorization'];
+
+  if(typeof bearerHeader !=='undefined'){
+
+    const bearer= bearerHeader.split('');
+    const bearerToken = bearer[1];
+    req.token= bearerToken;
+    next();
+
+
+  }else {
+    res.sendStatus(403)
+  }
+  
+
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port);
